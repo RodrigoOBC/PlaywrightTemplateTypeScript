281791c79816a6daa8fa85f9983d4a84d420be98
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-09-23 17:42:02 -0300

    Add form submission test for adding a person to the form

---
+++ tests/formPage.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import { FormPage } from './page/form.page';
+
+
+
+test('Adicione uma pessoa ao formulario', async ({ page }) => {
+  const formPage = new FormPage(page);
+  await formPage.navigate();
+  await page.pause()
+  await formPage.firstName.fillTextBox('Rodrigo');
+  await formPage.lastName.fillTextBox('Cabral');
+  await formPage.email.fillTextBox('teste@teste.com');
+  await formPage.selectGender('Male')
+  await formPage.mobileNumber.fillTextBox('1234567890');
+  // ... continue preenchendo o formulário conforme necessário
+  await formPage.submitForm();
+
+  // Verifique se o formulário foi enviado com sucesso
+  await expect(page.locator('#example-modal-sizes-title-lg')).toHaveText('Thanks for submitting the form');
+});
