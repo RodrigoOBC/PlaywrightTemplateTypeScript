fe5028dccfbffd5ce1de8836d00f8316ed733322
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-09-24 11:49:12 -0300

    Add ButtonComponent class for handling button interactions

---
+++ tests/components/button.components.ts
@@ -0,0 +1,28 @@
+import { type Locator, type Page } from '@playwright/test';
+
+
+export class ButtonComponent {
+    readonly page: Page
+    readonly locator: Locator
+
+    constructor(page: Page, private selector: string) {
+        this.page = page;
+        this.locator = this.page.getByRole('button', { name: this.selector });
+    }
+
+    async get() {
+        return this.locator
+    }
+
+    async click() {
+        const button = await this.get();
+        await button.click();
+    }
+
+    async isVisible() {
+        const button = await this.get();
+        return button.isVisible();
+    }
+
+
+}
