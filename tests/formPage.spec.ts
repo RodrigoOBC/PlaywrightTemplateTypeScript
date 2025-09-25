99e79d9add674d1f0edde7a3518199f9cf6d032a
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-09-25 17:59:43 -0300

    Refactor formPage.spec.ts to use studentData object for improved maintainability and readability

--- tests/formPage.spec.ts
+++ tests/formPage.spec.ts
@@ -1,20 +1,30 @@
 import { test, expect } from '@playwright/test';
 import { FormPage } from './page/form.page';
 
+const studentData = {
+  firstName: 'Rodrigo',
+  lastName: 'Cabral',
+  email: 'teste@teste.com',
+  gender: 'Male',
+  mobile: '1234567890',
+  subject: 'Computer Science',
+  state: 'NCR',
+  city: 'Delhi'
+};
 
 
 test('Registrar um estudante com sucesso', async ({ page }) => {
   const formPage = new FormPage(page);
   await formPage.navigate();
 
-  await formPage.firstName.fillTextBox('Rodrigo');
-  await formPage.lastName.fillTextBox('Cabral');
-  await formPage.email.fillTextBox('teste@teste.com');
-  await formPage.gender('Male').click();
-  await formPage.mobileNumber.fillTextBox('1234567890');
-  await formPage.subjectsInput.fillAndPressEnter('Computer Science');
-  await formPage.stateDropdown.clickAndSelectOption('NCR');
-  await formPage.cityDropdown.clickAndSelectOption('Delhi');
+  await formPage.firstName.fillTextBox(studentData.firstName);
+  await formPage.lastName.fillTextBox(studentData.lastName);
+  await formPage.email.fillTextBox(studentData.email);
+  await formPage.gender(studentData.gender).click();
+  await formPage.mobileNumber.fillTextBox(studentData.mobile);
+  await formPage.subjectsInput.fillAndPressEnter(studentData.subject);
+  await formPage.stateDropdown.clickAndSelectOption(studentData.state);
+  await formPage.cityDropdown.clickAndSelectOption(studentData.city);
   // ... continue preenchendo o formulário conforme necessário
   await formPage.submitButton.click();
 
