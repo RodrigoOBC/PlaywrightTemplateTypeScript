2a977bea5b24b9eee243fcc4f48c71e83d9fcac8
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-09-29 18:39:21 -0300

    Update baseURL in Playwright configuration for testing environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
 
-    baseURL: 'https://demoqa.com',
+    baseURL: 'https://opensource-demo.orangehrmlive.com/',
 
     trace: 'on-first-retry',
   },
