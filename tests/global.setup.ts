19133f3dac38b3fa43b90228f4144ac4c73ea8cb
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-10-03 11:36:55 -0300

    Update global setup to enhance login validation and store cookies for session management

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -1,10 +1,11 @@
 import { test as setup } from '@playwright/test';
 import { LoginPage } from './page/login.page';
 
-setup('create new database', async ({ page }) => {
+setup('Realizar login com sucesso', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.navigate();
   await loginPage.makeLogin('Admin', 'admin123');
- await loginPage.validateLogin();
-  await page.context().storageState({ path: 'teste/auth.json' });
+  await loginPage.validateLogin();
+  const cookies = await page.context().cookies();
+  process.env.COOKIES = JSON.stringify(cookies);
 });
