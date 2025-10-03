e7fd9d0e21139b07a86e9f3a4383ac654db6d2a8
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-10-03 13:46:06 -0300

    chore(cleanup): remove unused types and streamline sidebar components

--- tests/page/login.page.ts
+++ tests/page/login.page.ts
@@ -1,4 +1,4 @@
-import { expect, type Locator, type Page } from '@playwright/test';
+import { expect, type Page } from '@playwright/test';
 import TextBoxComponent from '../components/textBox.components';
 import { ButtonComponent, LinkButtonComponent } from '../components/button.components';
 
