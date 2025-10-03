f77ac18d8616d0b0c5fc6483b3190b4e0c93583e
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-10-03 11:37:23 -0300

    Refactor admin tests to improve structure and enhance user filtering functionality

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -1,13 +1,30 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './page/login.page';
-import { AdminPage } from './page/admin.page';
+import { AdminUserPage } from './page/admin.page';
 
 
-test.use({ storageState: 'teste/auth.json' });
+test.beforeEach(async ({ page }) => {
+  const deserializedCookies = JSON.parse(process.env.COOKIES)
+  await page.context().addCookies(deserializedCookies);
+});
 
+test.afterEach(async ({ page }) => {
+  await page.context().close();
+});
 
-test('Registrar um estudante com sucesso', async ({ page }) => { 
-  const adminPage = new AdminPage(page);
-  await adminPage.navigate();
-  await page.pause();
+const dataTest = [
+  { 'Username': 'QACabral', "User Role": 'ESS', 'Employee Name': 'Rodrigo QA Cabral', Status: 'Enabled', 'Actions': '' },]
+
+test('Filtrar com sucesso', async ({ page }) => {
+  const adminUserPage = new AdminUserPage(page);
+  await adminUserPage.navigate(); 
+  await adminUserPage.userName.fillTextBox(dataTest[0]['Username']);
+  await adminUserPage.userRoleComboBox.clickAndSelectOption(dataTest[0]['User Role']);
+  await adminUserPage.employeeNameTextBox.clickAndSelectOption(dataTest[0]['Employee Name']);
+  await adminUserPage.statusComboBox.clickAndSelectOption(dataTest[0]['Status']);
+  await adminUserPage.searchButton.click();
+  
+  await adminUserPage.table.waitForLoad();
+  await adminUserPage.validateUsersTable(dataTest);  
+
 });
