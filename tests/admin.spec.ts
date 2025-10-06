4d50d33d17c55f6323f16c0cbb65d2cfb5277c1e
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-10-06 10:33:03 -0300

    refactor(tests): restructure admin tests to utilize fixtures for cookie management and improve test readability

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -1,30 +1,29 @@
-import { test } from '@playwright/test';
+import { test, expect } from './fixtures/fixtures';
 import { AdminUserPage } from './page/admin.page';
 
 
-test.beforeEach(async ({ page }) => {
-  const cookie: string = process.env.COOKIES? process.env.COOKIES : '';
-  const deserializedCookies = JSON.parse(cookie)
-  await page.context().addCookies(deserializedCookies);
-});
 
 test.afterEach(async ({ page }) => {
   await page.context().close();
 });
 
-const dataTest = [
-  { 'Username': 'QACabral', "User Role": 'ESS', 'Employee Name': 'Rodrigo QA Cabral', Status: 'Enabled', 'Actions': '' },]
+const CT01dataTest = [
+  { 'Username': 'JacobBrown', "User Role": 'ESS', 'Employee Name': 'Jacob Brown', 'Status': 'Enabled', 'Actions': '' }
+]
 
-test('Filtrar com sucesso', async ({ page }) => {
-  const adminUserPage = new AdminUserPage(page);
-  await adminUserPage.navigate();
-  await adminUserPage.userName.fillTextBox(dataTest[0]['Username']);
-  await adminUserPage.userRoleComboBox.clickAndSelectOption(dataTest[0]['User Role']);
-  await adminUserPage.employeeNameTextBox.clickAndSelectOption(dataTest[0]['Employee Name']);
-  await adminUserPage.statusComboBox.clickAndSelectOption(dataTest[0]['Status']);
-  await adminUserPage.searchButton.click();
+for (const dataTest of CT01dataTest) {
+  test(`Filtrar usuario ${dataTest['Username']}`, async ({ page, authCookies}) => {
+    await page.context().addCookies(authCookies);
+    const adminUserPage = new AdminUserPage(page);
+    await adminUserPage.navigate();
+    await adminUserPage.userName.fillTextBox(dataTest['Username']);
+    await adminUserPage.userRoleComboBox.clickAndSelectOption(dataTest['User Role']);
+    await adminUserPage.employeeNameTextBox.clickAndSelectOption(dataTest['Employee Name']);
+    await adminUserPage.statusComboBox.clickAndSelectOption(dataTest['Status']);
+    await adminUserPage.searchButton.click();
 
-  await adminUserPage.table.waitForLoad();
-  await adminUserPage.validateUsersTable(dataTest);
+    await adminUserPage.table.waitForLoad();
+    await adminUserPage.validateUsersTable([dataTest]);
 
-});
+  });
+}
---
+++ tests/fixtures/fixtures.ts
@@ -0,0 +1,24 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from '../page/login.page'
+
+type MyFixtures = {
+  authCookies: any[];
+};
+
+
+
+export const test = base.extend<MyFixtures>({
+
+    authCookies: async ({ page }, use) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate();
+        await loginPage.makeLogin('Admin', 'admin123');
+        await loginPage.validateLogin();
+        
+        const cookies = await page.context().cookies();
+        await use(cookies)
+    }
+
+})
+
+export const expect = test.expect;
