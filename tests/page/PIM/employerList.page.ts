73d102538f3da1557f38497d237ffcb21afa0d83
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2025-10-14 17:17:13 -0300

    feat(page): add EmployerListPage component with navigation and form elements

---
+++ tests/page/PIM/employerList.page.ts
@@ -0,0 +1,51 @@
+import { expect, type Page } from '@playwright/test';
+import TextBoxComponent from '../../components/textBox.components';
+import { ButtonComponent, ButtonIconComponent } from '../../components/button.components';
+import { SideBarComponent } from '../../components/sideBar.components'
+import { ComboboxComponent, AutoCompleteComponent } from '../../components/combobox.components';
+import { Table } from '../../components/table.components';
+
+
+export class EmployerListPage {
+    readonly page: Page;
+    readonly sideBarComponent: SideBarComponent
+    readonly employerNameTextBox: AutoCompleteComponent
+    readonly employmentID: TextBoxComponent
+    readonly employmentStatusComboBox: ComboboxComponent
+    readonly employmentIncludeComboBox: ComboboxComponent
+    readonly supervisorNameTextBox: AutoCompleteComponent
+    readonly jobTitleComboBox: ComboboxComponent
+    readonly subUnitComboBox: ComboboxComponent
+    readonly searchButton: ButtonComponent
+    readonly table: Table
+
+    constructor(page: Page) {
+        this.page = page;
+        this.sideBarComponent = new SideBarComponent(this.page);
+        this.employerNameTextBox = new AutoCompleteComponent(this.page, { locatorObject: this.page.getByRole('textbox', { name: 'Type for hints...' }) });
+        this.employmentID = new TextBoxComponent(this.page, { locatorObject: this.page.getByRole('textbox').nth(1) });
+        this.employmentStatusComboBox = new ComboboxComponent(this.page, { locatorObject: this.page.locator('.oxd-select-text').first() });
+        this.employmentIncludeComboBox = new ComboboxComponent(this.page, { locatorObject: this.page.locator('.oxd-select-text').nth(1) });
+        this.supervisorNameTextBox = new AutoCompleteComponent(this.page, { locatorObject: this.page.getByRole('textbox').nth(2) });
+        this.jobTitleComboBox = new ComboboxComponent(this.page, { locatorObject: this.page.locator('.oxd-select-text').nth(2) });
+        this.subUnitComboBox = new ComboboxComponent(this.page, { locatorObject: this.page.locator('.oxd-select-text').nth(3) });
+        this.searchButton = new ButtonComponent(this.page, 'Search');
+        this.table = new Table(this.page,
+            {
+                columnsName: ['ID', 'First (& Middle) Name', 'Last Name', 'Job Title', 'Employment Status', 'Sub Unit', 'Supervisor', 'Actions'],
+                hasTextBox: false,
+                tableButtonActions: [
+                    { name: 'Edit', type: new ButtonIconComponent(this.page, 'edit') },
+                    { name: 'Delete', type: new ButtonIconComponent(this.page, 'delete') }
+                ]
+            }
+        );
+    }
+
+    async navigate(): Promise<void> {
+        await this.page.goto('/');
+        await this.sideBarComponent.pimButton.click();
+    }
+
+    
+}
