b3ae10ce0a05675442bebc92e9a2135d59555530
rodrigo.cabral <rodrigo.cabral.qa@gmail.com>
2025-11-21 16:40:11 -0300

    fix: remove .only from CT02 test to ensure all tests run

--- tests/specs/admin.spec.ts
+++ tests/specs/admin.spec.ts
@@ -32,7 +32,7 @@ test(`CT01 - Create Admin Users`, async ({ page, createUserByTest: createCT01Tes
   }
 })
 
-test.only(`CT02 - Validate Search Admin Users in Admin List`, async ({ page, createUserByTest: createCT02TestData, adminTestData }) => {
+test(`CT02 - Validate Search Admin Users in Admin List`, async ({ page, createUserByTest: createCT02TestData, adminTestData }) => {
   const adminUserPage = new AdminUserPage(page);
   const preConditionData = { data: [
     { firstName: 'Teste', middleName: 'Cabral', lastName: 'Silva', empNumber: '891', employeeId: '0891', loginDetails: true, userName: 'TesteCabral', status: 'Enabled', password: 'Teste@1234', confirmPassword: 'Teste@1234' },
